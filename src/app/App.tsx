72bfb9ae97d629c675fa05a23223cdc34855b574
mshns <mishanos412@gmail.com>
2023-03-12 06:29:13 +0300

    feat: create app architecture

---
+++ src/app/App.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import './App.scss';
+
+class App extends React.Component {
+  render() {
+    return (
+      <div className="App">
+        <h1>Vite + React</h1>
+      </div>
+    );
+  }
+}
+
+export default App;
---
+++ src/pages/about/About.tsx
@@ -0,0 +1,13 @@
+import { Component } from 'react';
+
+class About extends Component {
+  render() {
+    return (
+      <div className="App">
+        <h1>Vite + React</h1>
+      </div>
+    );
+  }
+}
+
+export default About;
