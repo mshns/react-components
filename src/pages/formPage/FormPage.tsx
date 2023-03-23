d3dc4201e24e17c2def15a232b5e94a5cd73981d
mshns <mishanos412@gmail.com>
2023-03-23 14:40:32 +0300

    feat: add refs for inputs

--- src/pages/formPage/FormPage.tsx
+++ src/pages/formPage/FormPage.tsx
@@ -1,12 +1,54 @@
 import React from 'react';
 
 import Form from '../../widgets/form/Form';
+import Card from '../../widgets/card/Card';
 
-class FormPage extends React.Component {
+import styles from './FormPage.module.scss';
+import { IProduct } from '../../app/types/interfaces';
+
+export type IFormPageState = {
+  cards: IProduct[];
+};
+
+class FormPage extends React.Component<Record<string, never>, IFormPageState> {
+  constructor(props: Record<string, never>) {
+    super(props);
+
+    this.state = {
+      cards: [
+        {
+          id: 1,
+          title: 'iPhone 13 Pro',
+          date: '2023-03-24',
+          discount: false,
+          brand: 'Apple',
+          category: 'Smartphones',
+          thumbnail:
+            'https://raw.githubusercontent.com/mshns/online-store/develop/src/assets/data/1/thumbnail.jpg',
+        },
+      ],
+    };
+  }
+
   render() {
     return (
-      <main className="main">
-        <Form />
+      <main className={styles.main}>
+        <section className={styles.section}>
+          <Form cards={this.state.cards} />
+        </section>
+        <section className={styles.section}>
+          {this.state.cards.map((item) => (
+            <Card
+              title={item.title}
+              date={item.date}
+              discount={item.discount}
+              brand={item.brand}
+              category={item.category}
+              thumbnail={item.thumbnail}
+              key={item.id}
+            />
+          ))}
+        </section>
       </main>
     );
   }
--- src/widgets/form/Form.tsx
+++ src/widgets/form/Form.tsx
@@ -1,32 +1,62 @@
 import React from 'react';
+import { IFormPageState } from '../../pages/formPage/FormPage';
 
-class Form extends React.Component {
-  constructor(props: Record<string, never>) {
+import styles from './Form.module.scss';
+
+class Form extends React.Component<IFormPageState> {
+  constructor(props: IFormPageState) {
     super(props);
 
-    this.state = {
-      searchInputValue: localStorage.getItem('searchInputValue') ?? '',
-    };
-
-    this.handleChange = this.handleChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
   }
 
   titleRef = React.createRef<HTMLInputElement>();
   dateRef = React.createRef<HTMLInputElement>();
+  brandRef = React.createRef<HTMLSelectElement>();
+  firstCategoryRef = React.createRef<HTMLInputElement>();
+  secondCategoryRef = React.createRef<HTMLInputElement>();
+  firstDiscountedRef = React.createRef<HTMLInputElement>();
+  secondDiscountedRef = React.createRef<HTMLInputElement>();
+  thumbnailRef = React.createRef<HTMLInputElement>();
 
-  handleChange() {
-    console.log(this.titleRef.current?.value);
-    console.log(this.dateRef.current?.value);
+  handleSubmit(event: React.FormEvent<HTMLFormElement>) {
+    event.preventDefault();
+    // console.log(this.titleRef.current?.value);
+    // console.log(this.dateRef.current?.value);
+    // console.log(this.brandRef.current?.value);
+    // console.log(this.firstCategoryRef.current?.checked);
+    // console.log(this.secondCategoryRef.current?.checked);
+    // console.log(this.firstDiscountedRef.current?.checked);
+    // console.log(this.secondDiscountedRef.current?.checked);
+    // console.log(this.thumbnailRef?.current?.files);
+
+    this.setState({
+      cards: [
+        ...this.props.cards,
+        {
+          id: 2,
+          title: 'iPhone 14 Pro',
+          date: '2023-03-24',
+          discount: false,
+          brand: 'Apple',
+          category: 'Smartphones',
+          thumbnail:
+            'https://raw.githubusercontent.com/mshns/online-store/develop/src/assets/data/1/thumbnail.jpg',
+        },
+      ],
+    });
+
+    console.log(this.props.cards);
   }
 
   render() {
     return (
-      <form>
+      <form className={styles.form} onSubmit={this.handleSubmit}>
         <fieldset>
           <legend>Product card</legend>
           <fieldset>
             <legend>Product title</legend>
-            <input ref={this.titleRef} type="text" onChange={this.handleChange} />
+            <input ref={this.titleRef} type="text" />
           </fieldset>
 
           <fieldset>
@@ -36,33 +66,33 @@ class Form extends React.Component {
 
           <fieldset>
             <legend>Brand</legend>
-            <select>
-              <option value="javascript">Xiaomi</option>
-              <option value="php">Apple</option>
+            <select ref={this.brandRef}>
+              <option value="Xiaomi">Xiaomi</option>
+              <option value="Apple">Apple</option>
             </select>
           </fieldset>
 
           <fieldset>
             <legend>Category</legend>
-            <input type="checkbox" id="smartphones" />
+            <input ref={this.firstCategoryRef} type="checkbox" id="smartphones" />
             <label htmlFor="smartphones">Smartphones</label>
 
-            <input type="checkbox" id="laptops" />
+            <input ref={this.secondCategoryRef} type="checkbox" id="laptops" />
             <label htmlFor="laptops">Laptops</label>
           </fieldset>
 
           <fieldset>
             <legend>Discounted</legend>
-            <input type="radio" id="yes" name="discounted" />
+            <input ref={this.firstDiscountedRef} type="radio" id="yes" name="discounted" />
             <label htmlFor="yes">Yes</label>
 
-            <input type="radio" id="no" name="discounted" />
+            <input ref={this.secondDiscountedRef} type="radio" id="no" name="discounted" />
             <label htmlFor="no">No</label>
           </fieldset>
 
           <fieldset>
             <legend>Image</legend>
-            <input type="file" accept="image/png, image/jpeg" />
+            <input ref={this.thumbnailRef} type="file" accept="image/png, image/jpeg" />
           </fieldset>
 
           <button type="submit">Create a card</button>
