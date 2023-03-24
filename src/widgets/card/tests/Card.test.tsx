6abdef83b11882d940755980ea057e52a3b14e44
mshns <mishanos412@gmail.com>
2023-03-24 21:14:24 +0300

    fix: change card props in card.test

--- src/widgets/card/tests/Card.test.tsx
+++ src/widgets/card/tests/Card.test.tsx
@@ -8,13 +8,9 @@ describe('Card', () => {
     render(
       <Card
         title={'Realme 10 4G'}
-        description={
-          'Best performance in its class. The best display in its class. Best in class. Your need for power has been satisfied.'
-        }
-        price={249}
-        discount={15}
+        date={'2023-03-24'}
+        discount={true}
         brand={'Realme'}
-        category={'Smartphones'}
         thumbnail={
           'https://raw.githubusercontent.com/mshns/online-store/develop/src/assets/data/3/thumbnail.jpg'
         }
@@ -31,13 +27,9 @@ describe('Card', () => {
     render(
       <Card
         title={'Xiaomi 12 Pro'}
-        description={
-          'Every precious moment in life deserves the best shot whether it in motion or still, bright or dark. We are ready to make moments mega, and so should you.'
-        }
-        price={899}
-        discount={17}
+        date={'2023-01-20'}
+        discount={false}
         brand={'Xiaomi'}
-        category={'Smartphones'}
         thumbnail={
           'https://raw.githubusercontent.com/mshns/online-store/develop/src/assets/data/2/thumbnail.jpg'
         }
