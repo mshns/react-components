7291bec93cd2fed01bc17d0a3f87a2a2d2c590cb
mshns <mishanos412@gmail.com>
2023-03-28 19:45:30 +0300

    refactor: convert formpage and form to functional

--- src/pages/formPage/FormPage.tsx
+++ src/pages/formPage/FormPage.tsx
@@ -1,39 +1,27 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 import Form from '../../widgets/form/Form';
 import Card from '../../widgets/card/Card';
 
 import styles from './FormPage.module.scss';
 
-import { IProduct, IProductList } from '../../app/types/interfaces';
+import { IProduct } from '../../app/types/interfaces';
 
-class FormPage extends React.Component<Record<string, unknown>, IProductList> {
-  constructor(props: Record<string, unknown>) {
-    super(props);
+const FormPage = () => {
+  const [productList, setProductList] = useState<IProduct[]>([]);
 
-    this.state = {
-      productList: [],
-    };
-  }
-
-  addProduct = (product: IProduct) => {
-    this.setState((prev) => ({ productList: [...prev.productList, product] }));
-  };
-
-  render() {
-    return (
-      <main className={styles.main}>
-        <section className={styles.section}>
-          <Form addProduct={this.addProduct} />
-        </section>
-        <section className={styles.section}>
-          {this.state.productList.map((item: IProduct) => (
-            <Card card={item} key={item.id} />
-          ))}
-        </section>
-      </main>
-    );
-  }
-}
+  return (
+    <main className={styles.main}>
+      <section className={styles.section}>
+        <Form setProductList={setProductList} />
+      </section>
+      <section className={styles.section}>
+        {productList.map((item: IProduct) => (
+          <Card card={item} key={item.id} />
+        ))}
+      </section>
+    </main>
+  );
+};
 
 export default FormPage;
--- src/widgets/form/Form.tsx
+++ src/widgets/form/Form.tsx
@@ -1,63 +1,54 @@
-import React from 'react';
+import React, { useRef, useState } from 'react';
 
 import BrandList from './components/BrandList';
 
 import styles from './Form.module.scss';
 
-import { IFormProps, IFormState } from './types/interfaces';
+import { IFormProps } from './types/interfaces';
 
 import { ValidationAlert, SubmitAlert } from './constants/messages';
 import todayDate from './helpers/today';
 
-class Form extends React.Component<IFormProps, IFormState> {
-  constructor(props: IFormProps) {
-    super(props);
+const Form = ({ setProductList }: IFormProps) => {
+  const formRef = useRef<HTMLFormElement | null>(null);
+  const titleRef = useRef<HTMLInputElement | null>(null);
+  const dateRef = useRef<HTMLInputElement | null>(null);
+  const brandRef = useRef<HTMLSelectElement | null>(null);
+  const withDiscountRef = useRef<HTMLInputElement | null>(null);
+  const withoutDiscountRef = useRef<HTMLInputElement | null>(null);
+  const thumbnailRef = useRef<HTMLInputElement | null>(null);
+  const agreeRef = useRef<HTMLInputElement | null>(null);
 
-    this.state = {
-      alertTitle: false,
-      alertDate: false,
-      alertBrand: false,
-      alertDiscount: false,
-      alertThumbnail: false,
-      alertAgree: false,
-      id: 1,
-      message: false,
-    };
+  const [alertTitle, setAlertTitle] = useState(false);
+  const [alertDate, setAlertDate] = useState(false);
+  const [alertBrand, setAlertBrand] = useState(false);
+  const [alertDiscount, setAlertDiscount] = useState(false);
+  const [alertThumbnail, setAlertThumbnail] = useState(false);
+  const [alertAgree, setAlertAgree] = useState(false);
+  const [id, setId] = useState(1);
+  const [message, setMessage] = useState(false);
 
-    this.handleSubmit = this.handleSubmit.bind(this);
-    this.handleChange = this.handleChange.bind(this);
-  }
-
-  formRef = React.createRef<HTMLFormElement>();
-  titleRef = React.createRef<HTMLInputElement>();
-  dateRef = React.createRef<HTMLInputElement>();
-  brandRef = React.createRef<HTMLSelectElement>();
-  withDiscountRef = React.createRef<HTMLInputElement>();
-  withoutDiscountRef = React.createRef<HTMLInputElement>();
-  thumbnailRef = React.createRef<HTMLInputElement>();
-  agreeRef = React.createRef<HTMLInputElement>();
-
-  handleSubmit(event: React.FormEvent<HTMLFormElement>) {
+  const handleSubmit = (event: React.FormEvent<HTMLFormElement>) => {
     event.preventDefault();
 
-    const titleValidation = this.titleRef.current?.value && this.titleRef.current?.value.length > 4;
-    this.setState({ alertTitle: titleValidation ? false : true });
+    const titleValidation = titleRef.current?.value && titleRef.current?.value.length > 4;
+    setAlertTitle(titleValidation ? false : true);
 
-    const dateValidation = this.dateRef.current?.value && this.dateRef.current?.value <= todayDate;
-    this.setState({ alertDate: dateValidation ? false : true });
+    const dateValidation = dateRef.current?.value && dateRef.current?.value <= todayDate;
+    setAlertDate(dateValidation ? false : true);
 
-    const brandValidation = this.brandRef.current?.value != '';
-    this.setState({ alertBrand: brandValidation ? false : true });
+    const brandValidation = brandRef.current?.value != '';
+    setAlertBrand(brandValidation ? false : true);
 
     const discountValidation =
-      this.withDiscountRef.current?.checked || this.withoutDiscountRef.current?.checked;
-    this.setState({ alertDiscount: discountValidation ? false : true });
+      withDiscountRef.current?.checked || withoutDiscountRef.current?.checked;
+    setAlertDiscount(discountValidation ? false : true);
 
-    const thumbnailValidation = this.thumbnailRef?.current?.files?.length;
-    this.setState({ alertThumbnail: thumbnailValidation ? false : true });
+    const thumbnailValidation = thumbnailRef?.current?.files?.length;
+    setAlertThumbnail(thumbnailValidation ? false : true);
 
-    const agreeValidation = this.agreeRef.current?.checked;
-    this.setState({ alertAgree: agreeValidation ? false : true });
+    const agreeValidation = agreeRef.current?.checked;
+    setAlertAgree(agreeValidation ? false : true);
 
     if (
       titleValidation &&
@@ -67,103 +58,95 @@ class Form extends React.Component<IFormProps, IFormState> {
       thumbnailValidation &&
       agreeValidation
     ) {
-      this.props.addProduct({
-        id: this.state.id,
-        title: this.titleRef.current?.value ?? '',
-        date: this.dateRef.current?.value ?? '',
-        discount: this.withDiscountRef.current?.checked ?? false,
-        brand: this.brandRef.current?.value ?? '',
-        thumbnail: this.thumbnailRef?.current?.files
-          ? URL.createObjectURL(this.thumbnailRef.current.files[0])
-          : '',
-      });
+      setProductList((prev) => [
+        ...prev,
+        {
+          id: id,
+          title: titleRef.current?.value ?? '',
+          date: dateRef.current?.value ?? '',
+          discount: withDiscountRef.current?.checked ?? false,
+          brand: brandRef.current?.value ?? '',
+          thumbnail: thumbnailRef?.current?.files
+            ? URL.createObjectURL(thumbnailRef.current.files[0])
+            : '',
+        },
+      ]);
 
-      this.setState({ message: true });
-      this.formRef.current?.reset();
-      this.setState((prev) => ({ id: prev.id + 1 }));
+      setMessage(true);
+      formRef.current?.reset();
+      setId((prev) => prev + 1);
     }
-  }
+  };
 
-  handleChange() {
-    this.setState({ message: false });
-  }
+  const handleChange = () => {
+    setMessage(false);
+  };
 
-  render() {
-    return (
-      <form ref={this.formRef} onSubmit={this.handleSubmit} onChange={this.handleChange}>
-        <fieldset className={styles.form}>
-          <legend className={styles.form_title}>Product card</legend>
-          <fieldset className={styles.form_field}>
-            <legend className={styles.form_subtitle}>Product name</legend>
-            <input
-              ref={this.titleRef}
-              type="text"
-              className={styles.form_input}
-              placeholder="Enter product name"
-            />
-            {this.state.alertTitle && <p className={styles.form_alert}>{ValidationAlert.Title}</p>}
-          </fieldset>
-
-          <fieldset className={styles.form_field}>
-            <legend className={styles.form_subtitle}>Release date</legend>
-            <input ref={this.dateRef} type="date" className={styles.form_input} />
-            {this.state.alertDate && <p className={styles.form_alert}>{ValidationAlert.Date}</p>}
-          </fieldset>
-
-          <fieldset className={styles.form_field}>
-            <legend className={styles.form_subtitle}>Product brand</legend>
-            <select ref={this.brandRef} className={styles.form_input}>
-              <BrandList />
-            </select>
-            {this.state.alertBrand && <p className={styles.form_alert}>{ValidationAlert.Brand}</p>}
-          </fieldset>
-
-          <fieldset className={styles.form_field}>
-            <legend className={styles.form_subtitle}>Discounted</legend>
-            <input ref={this.withDiscountRef} type="radio" id="withDiscount" name="discount" />
-            <label htmlFor="withDiscount">On sale</label>
-            <input
-              ref={this.withoutDiscountRef}
-              type="radio"
-              id="withoutDiscount"
-              name="discount"
-            />
-            <label htmlFor="withoutDiscount">Without discount</label>
-            {this.state.alertDiscount && (
-              <p className={styles.form_alert}>{ValidationAlert.Discount}</p>
-            )}
-          </fieldset>
-
-          <fieldset className={styles.form_field}>
-            <legend className={styles.form_subtitle}>Product image</legend>
-            <input ref={this.thumbnailRef} type="file" accept="image/png, image/jpeg" />
-            {this.state.alertThumbnail && (
-              <p className={styles.form_alert}>{ValidationAlert.Thumbnail}</p>
-            )}
-          </fieldset>
-
-          <fieldset className={styles.form_field}>
-            <legend className={styles.form_subtitle}>Agreement</legend>
-            <input ref={this.agreeRef} type="checkbox" id="agree" />
-            <label htmlFor="agree">I consent to the processing of data</label>
-            {this.state.alertAgree && <p className={styles.form_alert}>{ValidationAlert.Agree}</p>}
-          </fieldset>
-
-          {this.state.message && (
-            <p className={styles.form_message}>
-              {SubmitAlert.Success}
-              <br />
-              {SubmitAlert.Another}
-            </p>
-          )}
-
-          <button className={styles.form_button} type="submit">
-            Create a card
-          </button>
+  return (
+    <form ref={formRef} onSubmit={handleSubmit} onChange={handleChange}>
+      <fieldset className={styles.form}>
+        <legend className={styles.form_title}>Product card</legend>
+        <fieldset className={styles.form_field}>
+          <legend className={styles.form_subtitle}>Product name</legend>
+          <input
+            ref={titleRef}
+            type="text"
+            className={styles.form_input}
+            placeholder="Enter product name"
+          />
+          {alertTitle && <p className={styles.form_alert}>{ValidationAlert.Title}</p>}
         </fieldset>
-      </form>
-    );
-  }
-}
+
+        <fieldset className={styles.form_field}>
+          <legend className={styles.form_subtitle}>Release date</legend>
+          <input ref={dateRef} type="date" className={styles.form_input} />
+          {alertDate && <p className={styles.form_alert}>{ValidationAlert.Date}</p>}
+        </fieldset>
+
+        <fieldset className={styles.form_field}>
+          <legend className={styles.form_subtitle}>Product brand</legend>
+          <select ref={brandRef} className={styles.form_input}>
+            <BrandList />
+          </select>
+          {alertBrand && <p className={styles.form_alert}>{ValidationAlert.Brand}</p>}
+        </fieldset>
+
+        <fieldset className={styles.form_field}>
+          <legend className={styles.form_subtitle}>Discounted</legend>
+          <input ref={withDiscountRef} type="radio" id="withDiscount" name="discount" />
+          <label htmlFor="withDiscount">On sale</label>
+          <input ref={withoutDiscountRef} type="radio" id="withoutDiscount" name="discount" />
+          <label htmlFor="withoutDiscount">Without discount</label>
+          {alertDiscount && <p className={styles.form_alert}>{ValidationAlert.Discount}</p>}
+        </fieldset>
+
+        <fieldset className={styles.form_field}>
+          <legend className={styles.form_subtitle}>Product image</legend>
+          <input ref={thumbnailRef} type="file" accept="image/png, image/jpeg" />
+          {alertThumbnail && <p className={styles.form_alert}>{ValidationAlert.Thumbnail}</p>}
+        </fieldset>
+
+        <fieldset className={styles.form_field}>
+          <legend className={styles.form_subtitle}>Agreement</legend>
+          <input ref={agreeRef} type="checkbox" id="agree" />
+          <label htmlFor="agree">I consent to the processing of data</label>
+          {alertAgree && <p className={styles.form_alert}>{ValidationAlert.Agree}</p>}
+        </fieldset>
+
+        {message && (
+          <p className={styles.form_message}>
+            {SubmitAlert.Success}
+            <br />
+            {SubmitAlert.Another}
+          </p>
+        )}
+
+        <button className={styles.form_button} type="submit">
+          Create a card
+        </button>
+      </fieldset>
+    </form>
+  );
+};
 
 export default Form;
