15df09fa4d249560fa662440bc6538a1469ae86c
mshns <mishanos412@gmail.com>
2023-04-20 00:08:28 +0300

    feat: add entry client and server

---
+++ src/entry-client.tsx
@@ -0,0 +1,16 @@
+import { hydrateRoot } from 'react-dom/client';
+import { BrowserRouter } from 'react-router-dom';
+
+import App from './app/App';
+
+import { Provider } from 'react-redux';
+import { store } from './store/store';
+
+hydrateRoot(
+  document.getElementById('root')!,
+  <Provider store={store}>
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  </Provider>
+);
---
+++ src/entry-server.tsx
@@ -0,0 +1,14 @@
+import { RenderToPipeableStreamOptions, renderToPipeableStream } from 'react-dom/server';
+import { StaticRouter } from 'react-router-dom/server';
+
+import App from './app/App';
+
+export default function render(url: string, opts: RenderToPipeableStreamOptions) {
+  const stream = renderToPipeableStream(
+    <StaticRouter location={url}>
+      <App />
+    </StaticRouter>,
+    opts
+  );
+  return stream;
+}
