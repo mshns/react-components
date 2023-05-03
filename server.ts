c79ffdf838e5ec6f89472fb861529b56a1f9f1da
mshns <mishanos412@gmail.com>
2023-05-03 11:00:54 +0300

    fix: add types in server.ts

--- server.ts
+++ server.ts
@@ -1,9 +1,18 @@
+import { PipeableStream, RenderToPipeableStreamOptions } from 'react-dom/server';
+
 import express from 'express';
 import { createServer } from 'vite';
 
 import fs from 'fs';
 import path from 'node:path';
 import { fileURLToPath } from 'node:url';
+
+interface IRender {
+  render: (
+    url: string,
+    options: RenderToPipeableStreamOptions
+  ) => Promise<{ stream: PipeableStream; injectPreload: () => string }>;
+}
 
 const __dirname = path.dirname(fileURLToPath(import.meta.url));
 const indexHTML = path.resolve(__dirname, 'index.html');
@@ -22,7 +31,7 @@ async function startServer() {
       const transformHTML = await vite.transformIndexHtml(url, template);
       const [startHTML, endHTML] = transformHTML.split('<!--app-->');
 
-      const { render } = await vite.ssrLoadModule('./src/entry-server.tsx');
+      const { render } = (await vite.ssrLoadModule('./src/entry-server.tsx')) as IRender;
 
       try {
         response.write(startHTML);
@@ -36,10 +45,10 @@ async function startServer() {
             response.end();
           },
         });
-      } catch (error) {
+      } catch (error: unknown) {
         console.error(error);
       }
-    } catch (error) {
+    } catch (error: unknown) {
       console.error(error);
     }
   });
